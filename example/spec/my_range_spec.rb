4ac1b6199547ef4b891534667cffd05b2494a4c0
Piotr Brych <pbrych@gmail.com>
2019-05-15 16:00:21 +0200

    First implementation

--- example/spec/my_range_spec.rb
+++ example/spec/my_range_spec.rb
@@ -4,12 +4,39 @@ RSpec.describe MyRange do
       MyRange.overlap?(left, right)
     end
 
-    context 'when there is no overlap' do
+    context 'when there is no overlap (case 1)' do
       let(:left)  { 1..2 }
       let(:right) { 3..4 }
 
       it 'returns false' do
         expect(apply).to be(false)
+      end
+    end
+
+    context 'when there is no overlap (case 2)' do
+      let(:left)  { 3..4 }
+      let(:right) { 1..2 }
+
+      it 'returns false' do
+        expect(apply).to be(false)
+      end
+    end
+
+    context 'when ranges overlap (case 1)' do
+      let(:left)  { 1..5 }
+      let(:right) { 3..7 }
+
+      it 'returns true' do
+        expect(apply).to be(true)
+      end
+    end
+
+    context 'when ranges overlap (case 2)' do
+      let(:left)  { 3..7 }
+      let(:right) { 1..5 }
+
+      it 'returns true' do
+        expect(apply).to be(true)
       end
     end
   end
