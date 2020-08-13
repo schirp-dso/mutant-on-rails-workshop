2f2223124b0ef72b5edb48e560116e989f63c6a6
Markus Schirp <mbj@schirp-dso.com>
2020-08-13 13:11:45 +0000

    Change to also require rails helper for env setup

--- solidus/core/mutant.sh
+++ solidus/core/mutant.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-exec bundle exec mutant           \
-  --include ./spec                \
-  --jobs 1                        \
-  --require ./spec/spec_helper.rb \
-  --use rspec                     \
+exec bundle exec mutant            \
+  --include ./spec                 \
+  --jobs 1                         \
+  --require ./spec/spec_helper.rb  \
+  --require ./spec/rails_helper.rb \
+  --use rspec                      \
   $*
